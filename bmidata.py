91339f5978d5f003f260c82dc79cfc249fde0d55
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-24 15:27:29 +0200

    connect excel doc to py for changes nd register

--- bmidata.py
+++ bmidata.py
@@ -1,12 +1,17 @@
+import openpyxl
+
 # Connect to excel
 filename = "database.xlsx"
+workbook = openpyxl.load_workbook(filename)
+sheet = workbook.active
+
 
 # User Input
-# first_name = input("First name: ")
-# surname = input("Surname: ")
-# mail = input("Email address: ")
-# height = input("Height in centimeters: ")
-# weight = input("Weight in kilograms: ")
+first_name = input("First name: ")
+surname = input("Surname: ")
+mail = input("Email address: ")
+height = input("Height in centimeters: ")
+weight = input("Weight in kilograms: ")
 
 
 
@@ -15,10 +20,13 @@ weight_kg = int(weight)
 height_cm = float(height)
 bmi = weight_kg / (height_cm / 100) ** 2
 user_bmi = int(bmi)
-print(f"Your BMI is: {user_bmi}")
+print(f"Dear {first_name} your BMI is: {user_bmi}")
 
 
 # Read Data from an Excel Worksheet in Python
 
 #Add data to excel
 sheet.append([first_name, surname, email, height_cm, weight_kg, user_bmi])
+
+#save data to excel
+workbook.save(filename)
