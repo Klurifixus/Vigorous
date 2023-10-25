a68b97971b194a790c8d46dbb2264a983539baee
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-25 11:12:27 +0200

    since probs with openpyx1 now trying spire

--- bmidata.py
+++ bmidata.py
@@ -1,11 +1,12 @@
-#libary to open excel
-import openpyxl
+# Libraries to open excel
+from spire.xls import Workbook
 
-# Connect to excel
-filename = "database.xlsx"
-workbook = openpyxl.load_workbook(filename)
-sheet = workbook.active
+# Create or load a workbook
+workbook = Workbook()
+workbook.LoadFromFile("database.xlsx")
 
+# Get the first worksheet
+worksheet = workbook.Worksheets[0]
 
 # User Input
 first_name = input("First name: ")
@@ -14,13 +15,12 @@ mail = input("Email address: ")
 height = input("Height in centimeters: ")
 weight = input("Weight in kilograms: ")
 
-# Autoget next availible user number
-if sheet.cell(row=2, column=1).value:
-    last_row = sheet.max_row
-    next_user_number = sheet.cell(row=last_row, column=1).value + 1
+# Autoget next available user number
+if worksheet.Range["A2"].Value:
+    last_row = worksheet.LastRow
+    next_user_number = worksheet.Range[f"A{last_row}"].NumberValue + 1
 else:
-    next_user_number = 1    
-
+    next_user_number = 1
 
 # Calculate BMI
 weight_kg = int(weight)
@@ -29,9 +29,15 @@ bmi = weight_kg / (height_cm / 100) ** 2
 user_bmi = int(bmi)
 print(f"Dear {first_name} your BMI is: {user_bmi}")
 
+# Add data to excel
+next_row = worksheet.LastRow + 1
+worksheet.Range[f"A{next_row}"].NumberValue = next_user_number
+worksheet.Range[f"B{next_row}"].Text = first_name
+worksheet.Range[f"C{next_row}"].Text = surname
+worksheet.Range[f"D{next_row}"].Text = mail
+worksheet.Range[f"E{next_row}"].NumberValue = height_cm
+worksheet.Range[f"F{next_row}"].NumberValue = weight_kg
+worksheet.Range[f"G{next_row}"].NumberValue = user_bmi
 
-#Add data to excel
-sheet.append([next_user_number, first_name, surname, mail, height_cm, weight_kg, user_bmi])
-
-#save data to excel
-workbook.save(filename)
+# Save data to excel
+workbook.SaveToFile("database.xlsx")
