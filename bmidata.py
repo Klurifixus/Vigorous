08bf8c934b4702bbacac108ecbcab861612ee0e1
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-25 13:43:45 +0200

    add openpyxl check for testing or info

--- bmidata.py
+++ bmidata.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
+
 # Load the Excel file into a DataFrame
 filename = "database.xlsx"
-
-
 df = pd.read_excel(filename, engine='openpyxl')
+
+
+
+#Openpyxl is installed? Check if it is installed!
+try:
+    import openpyxl
+except ImportError:
+    print("You need to install openpyxl to run this example")
+    exit()    
+
 
 # User Input
 first_name = input("First name: ")
@@ -13,6 +22,8 @@ mail = input("Email address: ")
 height = input("Height in centimeters: ")
 weight = input("Weight in kilograms: ")
 
+
+
 # Calculate BMI
 weight_kg = int(weight)
 height_cm = float(height)
@@ -20,11 +31,16 @@ bmi = weight_kg / (height_cm / 100) ** 2
 user_bmi = int(bmi)
 print(f"Dear {first_name} your BMI is: {user_bmi}")
 
+
+
 # Get the next available user number
 if not df.empty:
     next_user_number = df['user'].max() + 1
 else:
     next_user_number = 1
+
+
+
 
 # Append new data to the DataFrame
 new_data = {
@@ -33,11 +49,13 @@ new_data = {
     'surname': surname,
     'mail': mail,
     'weight_kg': weight_kg,
-    'length_cm': height_cm,
+    'height_cm': height_cm,
     'user_bmi': user_bmi
 }
 
 df = df.append(new_data, ignore_index=True)
+
+
 
 # Save updated data back to the Excel file
 df.to_excel(filename, index=False, engine='openpyxl')
