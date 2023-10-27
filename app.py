a01d4f15bba94b6c53a3138cbbbe8872154af1d1
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-27 13:09:17 +0200

    set up bmi calc in app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask,  request, render_template
+from flask import Flask, request, render_template
 import os
 import pandas as pd
-
 
 app = Flask(__name__)
 
@@ -14,24 +13,48 @@ def submit():
     first_name = request.form['first_name']
     surname = request.form['surname']
     mail = request.form['mail']
-    weight_kg = request.form['weight_kg']
-    height_cm = request.form['height_cm']
-    
-# Read existing data from the Excel file
-try:
-    df = pd.read_excel("database.xlsx")
-except FileExistsError
-    df = pd.DataFrame()
-    
-# Add new user data for BMI calculation 
-new_data = {'First Name': first_name,'Surname': [surname],'Mail': [mail], 'Weight in Kg': [weight_kg], 'Height in Cm': [height_cm]} 
-new_df['user_bmi'] = new_df.apply(lambda row: (row['Weight in Kg'] / (row['Height in Cm'] / 100) ** 2)), axis=1)
+    weight_kg = float(request.form['weight_kg'])
+    height_cm = float(request.form['height_cm'])
 
-# Convert the new data to a Pandas DataFrame
-new_data_df = pd.DataFrame(new_data)   
+    # Read existing data from the Excel file
+    try:
+        df = pd.read_excel("database.xlsx")
+    except FileNotFoundError:
+        df = pd.DataFrame()
 
-            
+    # Add new user data for BMI calculation
+    new_data = {
+        'First Name': [first_name],
+        'Surname': [surname],
+        'Mail': [mail],
+        'Weight in Kg': [weight_kg],
+        'Height in Cm': [height_cm]
+    }
+    new_df = pd.DataFrame(new_data)
+    new_df['user_bmi'] = new_df.apply(lambda row: (row['Weight in Kg'] / (row['Height in Cm'] / 100) ** 2), axis=1)
 
+    # BMI to 1 decimal
+    new_df['user_bmi'] = new_df['user_bmi'].round(1)
+
+    # Combine the new data with the existing data and save to the Excel file
+    df = pd.concat([df, new_df], ignore_index=True)
+    df.to_excel("database.xlsx", index=False)
+
+    # Calculate BMI status
+    bmi = new_df['user_bmi'].values[0]
+    category = ""
+    if bmi < 18.5:
+        category = "Underweight"
+    elif bmi >= 18.5 and bmi <= 24.9:
+        category = "Normal"
+    elif bmi >= 25 and bmi <= 29.9:
+        category = "Overweight"
+    else:
+        category = "Obese"
+
+    return f'Your BMI is {bmi} and your BMI category is {category}'
+
+# Run flask to Heroku
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
