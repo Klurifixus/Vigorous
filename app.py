7b2a4a65ac7c9e8b4afcfd6e5e2d8449a89fc2f7
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-27 12:11:16 +0200

    flask route for post request when submit

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit(): 
+    first_name = request.form['first_name']
+    surname = request.form['surname']
+    mail = request.form['mail']
+    weight_kg = request.form['weight_kg']
+    height_cm = request.form['height_cm']
+    
+    
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
