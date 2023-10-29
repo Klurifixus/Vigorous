45c96105b1e99e53f243ab4c58c5b9c024cf6c8e
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-29 10:34:58 +0100

    change path for flask: app.py( add static folder)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import os
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
